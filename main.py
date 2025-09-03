28e8394d07621f57a7a028c63f02b78f6b0ea455
SixtenGedda <sixten.gedda@gmail.com>
2025-09-03 16:44:12 +0200

    Fixed correct lib, changed api url and first working poc

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import urequests
+import requests
 
-AURORA_API_URL = "https://auroraforecast.space/api/kp/now"
+AURORA_API_URL = "https://services.swpc.noaa.gov/products/noaa-planetary-k-index-forecast.json"
 
-def fetch_aurora_data:
+def fetch_aurora_data():
+    response = requests.get(AURORA_API_URL)
+    data = response.json()
+    response.close()
+    return data
+
+print((fetch_aurora_data()))
     
